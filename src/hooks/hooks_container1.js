45c824d9ba6fa817cfaeb3a0388344126095eef1
gurujustin <meanjustin2020@hotmail.com>
2020-12-26 12:07:09 -0500

    UseEffect function and watch other value

--- src/hooks/hooks_container1.js
+++ src/hooks/hooks_container1.js
@@ -1,10 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 
 
 //main app 
 const HooksContainer1 = () => {
 
   const [stateValue, setValue] = useState(0)
+  const [useEffectValue, setUseEffectValue] = useState(null)
 
   const increasement = () => {
     setValue(stateValue + 1)
@@ -14,6 +15,14 @@ const HooksContainer1 = () => {
     setValue(stateValue - 1)
   }
 
+  useEffect(() => {
+    setTimeout(() => setUseEffectValue("UseEffect Worked"), 3000)
+  }, [stateValue])
+
+  const changeUseEffect = () => {
+    setUseEffectValue("Some text");
+  }
+
 
   return(
     <div>
@@ -21,8 +30,14 @@ const HooksContainer1 = () => {
       <br />
       <button onClick= { () => increasement()}> Inc Local State Value</button>
       <button onClick= { () => decreasement()}> Dec Local State Value</button>
+      <button onClick= { () => changeUseEffect()}> Change Use Effect value</button>
 
       <br />
+      {
+        useEffectValue
+        ? <p>{useEffectValue}</p>
+        : <p>No effect value</p>
+      }
       Local State Valuable : {stateValue}
     </div>
   )
